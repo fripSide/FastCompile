387cabff872170cae4eecf195c52ba6f4b43b859
fripSide <clangllvm@126.com>
2024-11-09 15:33:14 +0000

    init && finish simple parser

---
+++ build/build.sh
@@ -0,0 +1,2 @@
+bash build/prebuilts_download.sh
+./build.sh --product-name rk3568 --ccache
---
+++ build/pull_code.sh
@@ -0,0 +1,16 @@
+mkdir ~/bin
+curl https://gitee.com/oschina/repo/raw/fork_flow/repo-py3 -o ~/bin/repo
+chmod a+x ~/bin/repo
+pip3 install -i https://repo.huaweicloud.com/repository/pypi/simple requests
+echo PATH=~/bin:$PATH >> ~/.bashrc
+source ~/.bashrc
+
+mkdir -p /oh-dev/oh-5.1
+cd /oh-dev/oh-5.1
+git config --global user.email "clangllvm@126.com"
+git config --global user.name "fripSide"
+
+repo init -u https://gitee.com/openharmony/manifest.git -b refs/tags/OpenHarmony-v5.1.0-Release --no-repo-verify
+repo sync -c
+repo forall -c 'git lfs pull'
+
