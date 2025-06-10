5cff8a147ad0b52c861c9c86b1c1473ed8a89c98
fripSide <clangllvm@126.com>
2025-06-10 17:03:48 +0800

    update CI2-disable color

--- build/pull_code.sh
+++ build/pull_code.sh
@@ -4,12 +4,12 @@ chmod a+x ~/bin/repo
 pip3 install -i https://repo.huaweicloud.com/repository/pypi/simple requests
 echo PATH=~/bin:$PATH >> ~/.bashrc
 source ~/.bashrc
-cat ~/.bashrc
 
 mkdir -p ~/oh-5.1
 cd ~/oh-5.1
 git config --global user.email "clangllvm@126.com"
 git config --global user.name "fripSide"
+git config --global color.ui false
 
 ~/bin/repo init -u https://gitee.com/openharmony/manifest.git -b refs/tags/OpenHarmony-v5.1.0-Release --no-repo-verify
 ~/bin/repo sync -c
